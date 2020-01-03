c88f6f8eb312b5bfd03ea9c25886b65ef99f55e1
Aaron Holbrook <aaron@zeek.com>
2020-01-03 10:06:45 -0600

    Move users file references

--- src/users.sh
+++ src/users.sh
@@ -19,17 +19,17 @@ add_user_generate_key() {
     sudo -u $1 mkdir -m 700 /home/$1/.ssh
     sudo -u $1 touch /home/$1/.ssh/authorized_keys
 
-    sudo -u $1 ssh-keygen -t ed25519 -C "pusher@zeek.com"
+    sudo -u $1 ssh-keygen -t ed25519
     chmod 600 /home/$1/.ssh/authorized_keys
 }
 
 add_users_from_file() {
-    if [ ! -f users.txt ]; then
+    if [ ! -f config/users.txt ]; then
         echo "No users.txt file found, aborting setup script."
         exit 1
     fi
 
-    cat users.txt | while read line || [[ -n $line ]];
+    cat config/users.txt | while read line || [[ -n $line ]];
     do
         USER=$(echo "$line" | cut -d= -f1)
         KEY=$(echo "$line" | cut -d= -f2)
