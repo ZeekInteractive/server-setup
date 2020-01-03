6c30b8f4dcedbf298b71511edff3f1680a32adc6
Aaron Holbrook <aaron@zeek.com>
2020-01-03 10:27:21 -0600

    Add user prompt

--- bootstrap.sh
+++ bootstrap.sh
@@ -5,3 +5,10 @@ apt-get -y install zip unzip
 
 curl -sSLO https://github.com/ZeekInteractive/server-setup/archive/master.zip
 
+unzip master.zip
+mv server-setup-master server-setup
+
+echo 'Users to create: name="key":'
+read USERS
+
+echo $USERS > config/users.txt
