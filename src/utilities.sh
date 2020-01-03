f9851ae20e6358eb5cf2ba8688cd99c3c69c2b75
Aaron Holbrook <aaron@zeek.com>
2020-01-03 13:19:06 -0600

    Update ending message

--- src/utilities.sh
+++ src/utilities.sh
@@ -5,5 +5,14 @@ maybe_add_line_to_file() {
 }
 
 end_message() {
-    "Setup script completed. Login as user to confirm system is working and then restart system with: 'sudo shutdown -r now'."
+    echo "Add the following to your ./ssh/config file for easy system access:"
+    
+    echo ""
+    echo "Host alias"
+    echo "  HostName" $(curl -s ifconfig.me)
+    echo "  Port 5079"
+    echo "  User username"
+    echo ""
+
+    echo "Setup script completed. Login as user to confirm system is working and then restart system with: 'sudo shutdown -r now'."
 }
